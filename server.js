3b19cb77fc37b7b9872b5c52eea70901e5366191
Kunal <kunal.v.mandalia@gmail.com>
2017-01-25 17:35:35 +0000

    Update greeting

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
+  response.end("...and all the folks in it!\n");
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
