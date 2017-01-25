f07ed06b927579adbbede440e62451affd5c8a7c
Kunal <kunal.v.mandalia@gmail.com>
2017-01-25 18:51:51 +0000

    Test addTwoNumbers

--- app.test.js
+++ app.test.js
@@ -12,6 +12,20 @@ describe('App functions', function() {
 		it('should contain my name', function() {
 			assert.equal(sayHello.indexOf(name) !== -1, true);
     });
-
   });
+
+	describe('addTwoNumbers', function() {
+		var n1 = Math.random();
+		var n2 = Math.random();
+
+		var sum = appFunctions.addTwoNumbers(n1, n2);
+    it('should return a number', function() {
+			assert.equal(typeof(sum), 'number');
+    });
+
+		it('should return the sum', function() {
+			assert.equal(sum, n1+n2);
+    });
+  });
+
 });
