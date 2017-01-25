905884f53f5e7d971db122a07cae6048cd8692f7
Kunal <kunal.v.mandalia@gmail.com>
2017-01-25 16:52:35 +0000

    Remove default string

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 module.exports = {
-	sayHello: function(name='stranger') {
-		return 'hello ' + name;
+	sayHello: function(name) {
+		return name ? 'hello '.concat(name) : 'hey stranger';
 	},
 	addTwoNumbers: function(a, b) {
 		return (a + b);
